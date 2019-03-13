490ee443d974029e4c441775a9b3cd5194b9f0fa
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-03-13 16:32:53 -0400

    open footer links externally

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = (props) => {
   return (
     <footer>
-     <p><a href="https://robotspacefish.dev/">robotspacefish</a>! 2019</p>
-     <p>Dummy data provided by <a href="https://mockaroo.com">mockaroo</a></p>
+     <p><a href="https://robotspacefish.dev/" target="_blank">robotspacefish</a>! 2019</p>
+      <p>Dummy data provided by <a href="https://mockaroo.com" target="_blank">mockaroo</a></p>
     </footer>
   );
 };
