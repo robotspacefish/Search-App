b27dee2af007c87b7e9a82b114c3ccbe6d516542
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-03-16 21:25:40 -0400

    Updates

---
+++ precache-manifest.dafba908d5cbfe5fcef636caf1f019ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff44b26e4370764719b4",
+    "url": "/Search-App/static/css/main.a50bff01.chunk.css"
+  },
+  {
+    "revision": "ff44b26e4370764719b4",
+    "url": "/Search-App/static/js/main.7c7fba52.chunk.js"
+  },
+  {
+    "revision": "8b4e7aa971e9f5d7cc80",
+    "url": "/Search-App/static/js/runtime~main.4ea492c1.js"
+  },
+  {
+    "revision": "bda0f16bcd18e1a5b9c2",
+    "url": "/Search-App/static/css/2.dcfea580.chunk.css"
+  },
+  {
+    "revision": "bda0f16bcd18e1a5b9c2",
+    "url": "/Search-App/static/js/2.8750babf.chunk.js"
+  },
+  {
+    "revision": "d643eb4d5869e8aae57df30565f462b0",
+    "url": "/Search-App/index.html"
+  }
+];
