e8a5f219088ca7f5266905b5d9881663ce054613
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-03-21 10:50:00 -0400

    add github link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = (props) => {
   return (
     <footer>
-      <p><a href="https://robotspacefish.dev/" target="_blank" rel="noopener noreferrer">robotspacefish</a>! 2019</p>
+      <p><a href="https://github.com/robotspacefish/Search-App" target="_blank" rel="noopener noreferrer">github</a></p>
       <p>Dummy data provided by <a href="https://mockaroo.com" target="_blank" rel="noopener noreferrer">mockaroo</a></p>
     </footer>
   );
